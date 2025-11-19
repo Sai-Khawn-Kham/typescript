ae2ca49702b13bf3561da6661d9a4eea95b07761
Sai-Khawn-Kham <khawnkham222@gmail.com>
2025-11-19 14:38:39 +0630

    typing in ts: implicit and explicit

---
+++ 1-learn-ts/type-explicit.ts
@@ -0,0 +1,3 @@
+// Explicit Typing is the practice of manually specifying and declaring data types
+// for variables, function parameters, return types and other constructs
+// using type annotations, providing clear intentional type definitions in the code
---
+++ 1-learn-ts/type-implicit.ts
@@ -0,0 +1,3 @@
+// Implicit Typing (Type Inference)
+// It is a feature where the TypeScript compiler automatically determines and assigns data types to variables, function and expressions
+// based on their values and assigned values without requiring explicit type annotations from the developer
